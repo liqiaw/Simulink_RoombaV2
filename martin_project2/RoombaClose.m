6a61356268e6cbf7ea173d252f76c3f5475b6470
spm1200 <spmartin@temple.edu>
2017-10-31 17:04:28 -0400

    Add files via upload

---
+++ martin_project2/RoombaClose.m
@@ -0,0 +1,11 @@
+function RoombaClose
+persistent obj
+coder.extrinsic('evalin');
+coder.extrinsic('get');
+if isempty(obj)
+    obj = evalin('base','objWs');
+end
+fwrite(obj,7); %reset opcode
+fwrite(obj,173); %stop opcode
+fclose(obj);
+%delete(obj);
